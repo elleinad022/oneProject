ade25febdba23ef37fd786294406f8d0fd7c35c2
elleinad <janjaaan12@gmail.com>
2025-11-01 23:09:10 +0800

    Added authentication controller functions

--- server/server.js
+++ server/server.js
@@ -4,15 +4,19 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 
 import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({ credentials: true }));
 
+// API Endpoints
 app.get("/", (req, res) => res.send("API is Working"));
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => console.log(`Server started on PORT: ${port}`));
