4be476684344ba333db81a2364ddf19392a04592
elleinad <janjaaan12@gmail.com>
2026-02-02 23:47:28 +0800

    Added personal records model

---
+++ server/models/personalRecordModel.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const progressEntrySchema = new mongoose.Schema(
+  {
+    value: {
+      type: Number,
+      required: true,
+    },
+    loggedAt: { type: Date, default: Date.now },
+    note: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true },
+);
+
+const progressTrackerSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+      index: true,
+    },
+
+    exercise: {
+      type: String,
+      required: true,
+    },
+
+    unit: {
+      type: String,
+      enum: ["kg", "lbs", "reps", "minutes"],
+      required: true,
+    },
+    entries: [progressEntrySchema],
+  },
+  { timestamps: true },
+);
+
+progressTrackerSchema.index({ user: 1, exercise: 1 }, { unique: true });
+
+const progressTrackerModel =
+  mongoose.models.progressTracker ||
+  mongoose.model("progressTracker", progressTrackerSchema);
+
+export default progressTrackerModel;
