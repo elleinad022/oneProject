e4d5beb141811f466ca0a346722bd3430632ea1d
elleinad <janjaaan12@gmail.com>
2026-03-30 00:17:51 +0800

    Added trust proxy line in server.js

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ import workoutProgramRouter from "./routes/workoutPlanRoutes.js";
 import personalRecordRouter from "./routes/personalRecordRoutes.js";
 
 const app = express();
+app.set("trust proxy", 1);
 const port = process.env.PORT || 4000;
 connectDB();
 
